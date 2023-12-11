6a2b3cbb3edb52eddfd0a94251401524ee8a9765
Christian Crites <ccrites98@gmail.com>
2023-12-11 14:55:28 -0500

    updated to format output

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -4,14 +4,23 @@ public class Main {
     public static void main(String[] args) {
         String url = "jdbc:mysql://127.0.0.1:3306/northwind";
         String user = "root";
-        String password = "-wouldntuliketoknowweatherboy-";
-        String query = "SELECT * FROM Products";
+        String password = "-wouldntyouliketoknowweatherboy-";
+        String query = "SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM Products";
         try {
             Connection conn = DriverManager.getConnection(url, user, password);
             Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery(query);
             while (rs.next()) {
-                System.out.println(rs.getString(2));
+                int productID = rs.getInt("ProductID");
+                String productName = rs.getString("ProductName");
+                double unitPrice = rs.getDouble("UnitPrice");
+                int unitsInStock = rs.getInt("UnitsInStock");
+                //Now to stack the information
+                System.out.println("Id: " + productID);
+                System.out.println("Name: " + productName);
+                System.out.println("Price: " + unitPrice);
+                System.out.println("Stock: " + unitsInStock);
+                System.out.println("------------------");
             }
             rs.close();
             stmt.close();
